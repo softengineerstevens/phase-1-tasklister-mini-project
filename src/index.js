2100dbce438bb6a3f82c03319fc0717c7085ad8a
softengineerstevens <softengineerstevens@gmail.com>
2024-07-20 14:32:32 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // your code here
-});
+  const form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const task = document.querySelector('#new-task-description').value
+    const task_list = document.querySelector('#tasks')
+    const list_item = document.createElement('li')
+    list_item.textContent = task
+    task_list.append(list_item)
+  })
+})
